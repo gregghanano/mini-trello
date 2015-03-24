07e93d119fbd7a2fa5b30faded745d232ca90f3d
Gregg Hanano <gregg.hanano@gmail.com>
2015-03-24 15:15:32 -0700

    updated Procfile and mongoose.js

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
 
-mongoose.connect('mongodb://localhost/buckets');
+mongoose.connect('mongodb://heroku_app35176538:tc78m68mti10vmetluagbdlpaa@ds049171.mongolab.com:49171/heroku_app35176538');
 
 var models_path = path.join(__dirname, './../models');
 
